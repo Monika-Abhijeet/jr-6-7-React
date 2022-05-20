e806b64d8f4ee0f13401f6f708375502294a6594
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-05-20 12:50:45 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./components/home/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/about/about";
+import Contact from "./components/contact/contact";
+
 function App() {
   return (
     <div className="App">
-      <Header
-        title="Shopping application"
-        subtitle="one stop solution for all your groceries"
-      ></Header>
-      <Home></Home>
+      <BrowserRouter>
+        <Header
+          title="Shopping application"
+          subtitle="one stop solution for all your groceries"
+        ></Header>
+        <div style={{ marginTop: "12vh" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
       <Footer email="abc@gmail.com" address="bangalore"></Footer>
     </div>
   );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,23 @@
 import "./header.css";
-
+import { Link } from "react-router-dom";
 function Header(props) {
   return (
-    <div className="header-container d-flex flex-column justify-content-center align-items-center">
+    <div className="header-container d-flex flex-row justify-content-between align-items-center">
       <h3>{props.title}</h3>
-      <h5>{props.subtitle}</h5>
+      <ul
+        className="d-flex flex-row justify-content-around"
+        style={{ width: "30%" }}
+      >
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
     </div>
   );
 }
